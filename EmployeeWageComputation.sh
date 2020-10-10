56b21de848cd1865ddfab49844869a522fabe9e5
Shashi-dev94 <shash.engineer@gmail.com>
2020-10-10 18:24:03 +0530

    Added: UC2_EmpDailyWage

--- EmployeeWageComputation.sh
+++ EmployeeWageComputation.sh
@@ -1,10 +1,16 @@
 #!/bin/bash -x
 
 randomCheck=$((RANDOM%2))
+dailyHours=0
+WagePerHour=20
+
 if [ $randomCheck -eq 1 ]
 then
-	echo "Emloyee is Present"
+	echo "Employee is Present"
+	dailyHours=8
 else
 	echo "Employee is Absent"
 fi
 
+dailyWage=$(($dailyHours*$WagePerHour))
+echo $dailyWage
