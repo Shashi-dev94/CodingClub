a966c5c20a0987c29b0c0ebb174a9f1d2b756ee4
Shashi-dev94 <shash.engineer@gmail.com>
2020-10-12 11:59:12 +0530

    Added: EmpPartTime/Fulltime functionality

--- EmployeeWageComputation.sh
+++ EmployeeWageComputation.sh
@@ -1,15 +1,20 @@
 #!/bin/bash -x
 
-randomCheck=$((RANDOM%2))
+randomOne=$((RANDOM%2))
+randomTwo=$((RANDOM%2))
 dailyHours=0
 WagePerHour=20
 
-if [ $randomCheck -eq 1 ]
+if [ $randomOne -eq 1 -a $randomTwo -eq 1 ]
 then
-	echo "Employee is Present"
+	echo "Employee is Fulltime Present"
 	dailyHours=8
+elif [ $randomOne -eq 1 -o $randomTwo -eq 1 ]
+then
+	echo "Employee is PartTime Present"
+	dailyHours=4
 else
-	echo "Employee is Absent"
+	echo employee is absent
 fi
 
 dailyWage=$(($dailyHours*$WagePerHour))
